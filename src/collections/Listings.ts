6cd8049ca8ebaeaed72bedeb92740028a4d46e74
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2025-03-18 19:21:42 -0400

    added the main fields for the listings colleciton

--- src/collections/Listings.ts
+++ src/collections/Listings.ts
@@ -30,6 +30,56 @@ export const Listings: CollectionConfig = {
       required: true,
       label: "Opportunity Title",
     },
+    {
+        name: "location type",
+        required: true,
+        type: "select",
+        options: [
+            "Remote",
+            "In-person",
+            "Hybrid",
+            "Lifestyle",
+        ],
+    },
+    {
+        type: "row",
+        fields: [
+            {
+                name: "min hours",
+                label: "Minimum hours per week",
+                type: "number",
+                min: "0",
+                max: "168",
+            },
+            {
+                name: "max hours",
+                label: "Maximum hours per week",
+                type: "number",
+                min: "0",
+                max: "168",
+            },
+        ],
+    },
+    {
+        name: "description",
+        type: "richText",
+    },
+    {
+        name: "first step",
+        type: "richText",
+    },
+    {
+        name: "tags",
+        type: "select",
+        hasMany: true,
+        options: [
+            "environment",
+            "homelessness",
+            "food security",
+            "LGBTQ",
+            "consumer action",
+        ],
+    },
     // Rest of your fields
   ],
   timestamps: true,
