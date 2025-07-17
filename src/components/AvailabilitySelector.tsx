8082e1a11f3ceefd651bfb9915c3ae172294ced4
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2025-07-17 10:46:45 -0400

    wip - working on structure for react and editing.

--- src/components/AvailabilitySelector.tsx
+++ src/components/AvailabilitySelector.tsx
@@ -8,28 +8,49 @@ import cn from 'classnames'
 export const AvailabilitySelector = ({ path }) => {
   const { value, setValue } = useField({ path })
 
-  console.log('raw value:', value)
-
   const [availability, setAvailability] = useState(value)
+
+  const blocks = {
+    sun: [],
+    mon: [],
+    tue: [],
+    wed: [],
+    thu: [],
+    fri: [],
+    sat: [],
+  }
+
+  for (let block in availability) {
+  }
 
   const labels = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
 
+  const rows = []
+
+  const addTimeBlock = () => {
+  }
+
+{for (let i = 0; i < 7; i++) {
+  rows.push(
+    <div key={i} className={cn(styles.avsel_row)}>
+      <label>{labels[i]}</label>
+      <div 
+        role="button" 
+        className={cn(styles.avsel_add_button)}
+        onClick={() => addTimeBlock()}
+      >
+        +
+      </div>
+      <TimeBlock/>
+      <p>{availability[i]?.start}</p>
+    </div>
+  )
+}}
+
+
   return (
     <div>
-      {labels.map((label, index) => (
-        <div key={label} className={cn(styles.avsel_row)}>
-          <label>{label}</label>
-          <div 
-            role="button" 
-            className={cn(styles.avsel_add_button)}
-            onClick={() => addTimeBlock()}
-          >
-            +
-          </div>
-          <TimeBlock/>
-          <p>{index}</p>
-        </div>
-      ))}
+      {rows}
     </div>
   )
 }
