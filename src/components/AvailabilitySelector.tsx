600dae56a5d762af9560a7f22ee2c430a53f58bf
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2025-07-24 10:42:17 -0400

    re-structured everything into separate components with state mangagement in the parent component. Looking good! wip

--- src/components/AvailabilitySelector.tsx
+++ src/components/AvailabilitySelector.tsx
@@ -15,89 +15,108 @@ export const AvailabilitySelector = ({ path }) => {
   const [dbAvailability, setDbAvailability] = useState(value)
   const [showModal, setShowModal] = useState(false)
 
-  let availability = Array(7).fill().map(() => [])
-
-  for (let block of dbAvailability) {
-    let row = Math.floor(block.start / 24)
-    availability[row].push([block.start % 24, block.end % 24])
-  }
-
-  const Row = (row) => {
-    const [timeBlocks, setTimeBlocks] = useState(availability[row])
-    const handleAddTimeBlock = () => {
-      availability[row].push([8, 16])
-      setTimeBlocks(prevTimeBlocks => [ ...prevTimeBlocks, [8, 16]])
-      console.log(availability[row])
+  const [availability, setAvailability] = useState(() => {
+    const array = Array(7).fill().map(() => [])
+    for (let block of value || []) {
+      let row = Math.floor(block.start/24)
+      array[row].push([block.start % 24, block.end % 24])
     }
-    const labels = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
-    return (
-      <div key={row} className={cn(styles.avsel_row)}>
-        <label>{labels[row]}</label>
-        <div 
-          role="button" 
-          className={cn(styles.avsel_add_button)}
-          onClick={() => handleAddTimeBlock()}
-        >
-          +
-        </div>
-        {timeBlocks.map(
-          ([start, end]) => {
-            return (
-              TimeBlock(start, end)
-            )
-          }
-        )}
-      </div>
-    )
+    return array
+  })
+
+  const handleEditTimeBlock = () => {
+    setShowModal((showModal) => !showModal)
   }
 
-  const TimeBlock = (start, end) => {
-    const timeFormat = (num) => {
-      if (num < 12) {
-        return num + 'am'
-      } else if (num != 24) {
-        return num - 12 + 'pm'
-      } else {
-        return num - 12 + 'am'
-      }
-    }
-    return (
-      <div 
-        className={cn(styles.avsel_time_block)}
-        key={start}
-      >
-        <span>{timeFormat(start)}</span>
-        <span>-</span>
-        <span>{timeFormat(end)}</span>
-        <div 
-          role="button"
-          onClick={() => {setShowModal(!showModal)}}
-        >
-          edit
-        </div>
-      </div>
-    )
+  const handleAddTimeBlock = (rowIndex) => {
+    //availability[row].push([8, 16])
+    //setTimeBlocks(prevTimeBlocks => [ ...prevTimeBlocks, [8, 16]])
+    console.log("add time block to row " + rowIndex)
+    setAvailability((oldAvail) => {
+      const newAvail = [...oldAvail]
+      newAvail[rowIndex] = [
+        ...newAvail[rowIndex], 
+        [8, 16]
+      ]
+      return newAvail
+    })
   }
-
-  const EditModal = () => {
-    return (
-      <p>EDIT MODAL</p>
-    )
-  }
-
-  let rows = []
-
-  {for (let i = 0; i < 7; i++) {
-    rows.push(
-      Row(i)
-    )
-  }}
 
   return (
     <div>
-      {rows}
+      {availability.map((timeBlocks, rowIndex) => (
+        <Row
+          key={rowIndex}
+          rowIndex={rowIndex}
+          timeBlocks={timeBlocks}
+          handleAddTimeBlock={handleAddTimeBlock}
+          handleEditTimeBlock={handleEditTimeBlock}
+        />
+      ))}
       {showModal && <EditModal/>}
     </div>
+  )
+}
+
+const Row = ({rowIndex, timeBlocks, handleAddTimeBlock, handleEditTimeBlock}) => {
+  const labels = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
+  return (
+    <div key={rowIndex} className={cn(styles.avsel_row)}>
+      <label>{labels[rowIndex]}</label>
+      <div 
+        role="button" 
+        className={cn(styles.avsel_add_button)}
+        onClick={() => handleAddTimeBlock(rowIndex)}
+      >
+        +
+      </div>
+      {timeBlocks.map(
+        ([start, end]) => {
+          return (
+            <TimeBlock
+              key={start+end}
+              start={start}
+              end={end}
+              handleEditTimeBlock={handleEditTimeBlock}
+            />
+          )
+        }
+      )}
+    </div>
+  )
+}
+
+const TimeBlock = ({start, end, handleEditTimeBlock}) => {
+  const timeFormat = (num) => {
+    if (num < 12) {
+      return num + 'am'
+    } else if (num != 24) {
+      return num - 12 + 'pm'
+    } else {
+      return num - 12 + 'am'
+    }
+  }
+  return (
+    <div 
+      className={cn(styles.avsel_time_block)}
+    >
+      <span>{timeFormat(start)}</span>
+      <span>-</span>
+      <span>{timeFormat(end)}</span>
+      <div 
+        role="button"
+        onClick={handleEditTimeBlock}
+      >
+        edit
+      </div>
+    </div>
+  )
+}
+
+const EditModal = () => {
+  return (
+    <p>EDIT MODAL</p>
   )
 }
 
